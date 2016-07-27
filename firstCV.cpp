5b8ee0f7c7177da9d4c1b33efdb3a6fb387ec608
SiuChiang <swc25050@uga.edu>
2016-07-27 00:07:00 -0400

    Revert Changes

--- firstCV.cpp
+++
@@ -1,9 +0,0 @@
-#include <opencv2/opencv.hpp> //include file for every supported opencv function
-int main(int argc, char** argv) {
-	cv::Mat img = cv::imread(argv[1], -1);
-	if (img.empty()) return -1;
-	cv::namedWindow("example1", cv::WINDOW_AUTOSIZE);
-	cv::imshow("example1", img);
-	cv::waitKey(0);
-	cv::destroyWindow("example1");
-}
